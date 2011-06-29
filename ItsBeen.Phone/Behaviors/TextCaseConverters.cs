843ccb663d1e74be10c82b862a306646e087823b
Sean A. Hanley <yadyn@yahoo.com>
2011-06-29 16:35:30 -0500

    Some minor layout tweaks for the phone. Now follows a bit closer to the Metro style (app title is all caps and pivot pages and such are all lower case).

---
+++ ItsBeen.Phone/Behaviors/TextCaseConverters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ItsBeen.Phone.Behaviors
+{
+	public class ToUpperTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null || value.GetType() != typeof(String))
+				return value;
+			
+			string text = (string)value;
+			return text.ToUpper();
+		}
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+	public class ToLowerTextConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			if (value == null || value.GetType() != typeof(String))
+				return value;
+
+			string text = (string)value;
+			return text.ToLower();
+		}
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
